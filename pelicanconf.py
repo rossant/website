380d843e595d340035ab669bbafc81f89168f71b
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-09-23 20:42:39 +0200

    Added tags.

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,12 +11,16 @@ SITEURL = ''
 PATH = 'content'
 THEME = 'themes/pure'
 STATIC_PATHS = ['images', 'pdfs']
-DEFAULT_CATEGORY = ''
 DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_PAGES_ON_MENU  = False
 DEFAULT_DATE = 'fs'
 FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
 DEFAULT_PAGINATION = 3
+
+
+DEFAULT_CATEGORY = ''
+
+DIRECT_TEMPLATES = ('index', 'archives')
 
 ARTICLE_URL = '{slug}/'
 ARTICLE_SAVE_AS = '{slug}/index.html'
@@ -24,10 +28,19 @@ ARTICLE_SAVE_AS = '{slug}/index.html'
 PAGE_URL = '{slug}/index.html'
 PAGE_SAVE_AS = '{slug}/index.html'
 
-CATEGORY_SAVE_AS = ''
-TAG_SAVE_AS = ''
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
 TAGS_SAVE_AS = ''
+
+AUTHOR_URL = ''
 AUTHOR_SAVE_AS = ''
+
+CATEGORY_URL = ''
+CATEGORY_SAVE_AS = ''
+
+
+
+
 TWITTER_USERNAME = 'cyrillerossant'
 MENUITEMS = [('Home', '/'),
              ('Projects', '/projects/'),
