16fb45a88ee9e9f4394a74194784c262c64ad593
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-09-25 14:42:52 +0200

    Updating upload script.

--- upload.sh
+++ upload.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-cp -ar output/. ../rossant.github.io
-cd ../rossant.github.io
-git add --ignore-removal *
-git commit -am "Update." && git push
+if [ -n "$1" ]
+then
+	pelican
+	git status
+	git commit -am "$1" && git push
+	cp -ar output/. ../rossant.github.io
+	cd ../rossant.github.io
+	git add --ignore-removal *
+	git status
+	git commit -am "$1" && git push
+else
+	echo "Please provide a commit message."
+	exit 1
+fi
